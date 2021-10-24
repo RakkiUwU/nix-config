d0d951c448ab77399d5848dbd9ae4d71c3dbeb9c
Gabriel Fontes <eu@misterio.me>
2021-10-24 18:02:19 -0300

    Update default.nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,17 +1,11 @@
 { ... }: {
   nixpkgs.overlays = [
     (final: prev: {
-      /*
-      # wlroots fork with nvidia proprietary driver support
-      wlroots = prev.wlroots.overrideAttrs (oldAttrs: rec {
-        src = prev.fetchFromGitHub {
-          owner = "danvd";
-          repo = "wlroots-eglstreams";
-          rev = "225adced7c3c66de3dde95068111c5e982dd9d78";
-          sha256 = "sha256-0DhREZcCbu5lDbaQrNFQwkcw0AgZdYPEfo/U2E6XQK8=";
-        };
+      wlroots = prev.wlroots.overrideAttrs(old: {
+        postPatch = ''
+          sed -i 's/assert(argb8888 &&/assert(true || argb8888 ||/g' 'render/wlr_renderer.c'
+        '';
       });
-      */
     })
   ];
 }
